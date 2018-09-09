cc48b09531d8f25341f52268718e956d62413cb2
mmo <thetechpomelo@gmail.com>
2018-09-10 00:19:08 +0200

    better format

--- EuclideanAlgorithm/ggt.c
+++ EuclideanAlgorithm/ggt.c
@@ -8,17 +8,24 @@ int ggtCalls = 0;
 int ggt(int a, int b)
 {
 	ggtCalls++;
-	if (ggtCalls >= MAX_ITERATIONS){
+	if (ggtCalls >= MAX_ITERATIONS)
+	{
 		printf("num of Iterations required not allowed > %i\n", MAX_ITERATIONS);
 		return -1;
 	}
 
-	while (a > 0 && b > 0){
-		if (a == b){
+	while (a > 0 && b > 0)
+	{
+		if (a == b)
+		{
 			return a;
-		}else if (a > b){
+		}
+		else if (a > b)
+		{
 			return ggt(a-b, b);
-		}else {
+		}
+		else 
+		{
 			return ggt (a, b-a);
 		}
 
