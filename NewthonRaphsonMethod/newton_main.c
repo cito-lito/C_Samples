ffcf017bb74ffdd0fe447bb3b7b62b18e060728c
mmo <thetechpomelo@gmail.com>
2018-09-17 23:33:01 +0200

    fixed

--- NewthonRaphsonMethod/newton_main.c
+++ NewthonRaphsonMethod/newton_main.c
@@ -5,7 +5,7 @@
 int main(void)
 {
 	double x;
-(	printf("\nWelcome! we are going to calculate the root of an equation\n");
+	printf("\nWelcome! we are going to calculate the root of an equation\n");
 	printf("with the Newton-Raphson Method: Xn+1 = Xn - F(x)/F'(x)\n");
 	printf("The equation is : x^3-x-2\n");
 	printf("Enter the Xo Value:");
